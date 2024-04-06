df0829aecd760a9a86b13d59276d443e27d84174
Aidan y <aidan.j.yoder@gmail.com>
2024-04-06 09:55:06 -0400

    NA

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeamCode/Subsystems/ExampleGripperSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeamCode/Subsystems/ExampleGripperSubsystem.java
@@ -5,7 +5,7 @@ import com.arcrobotics.ftclib.command.SubsystemBase;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 
-/**
+/*
  * A gripper mechanism that grabs a stone from the quarry.
  * Centered around the Skystone game for FTC that was done in the 2019
  * to 2020 season.
@@ -18,14 +18,14 @@ public class ExampleGripperSubsystem extends SubsystemBase {
         mechRotation = hMap.get(Servo.class, name);
     }
 
-    /**
+    /*
      * Grabs a stone.
      */
     public void grab() {
         mechRotation.setPosition(0.76);
     }
 
-    /**
+    /*
      * Releases a stone.
      */
     public void release() {
