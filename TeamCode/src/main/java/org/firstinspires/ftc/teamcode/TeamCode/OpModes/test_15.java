78d103512ca4aa14f47aaf43598b27a819f8c9ae
TornadoBoah <nboekekitt@gmail.com>
2024-04-22 13:54:17 -0400

    Updated some code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeamCode/OpModes/test_15.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeamCode/OpModes/test_15.java
@@ -4,6 +4,7 @@ package org.firstinspires.ftc.teamcode.TeamCode.OpModes;
 import com.arcrobotics.ftclib.command.CommandOpMode;
 import com.arcrobotics.ftclib.gamepad.GamepadEx;
 import com.arcrobotics.ftclib.gamepad.GamepadKeys;
+import com.qualcomm.hardware.rev.RevTouchSensor;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 
@@ -17,16 +18,28 @@ public class test_15 extends CommandOpMode {
     private GamepadEx gamepadB;
     private IntakeSubsystem15 Intake15455;
     private PassSubsystem15 pass15455;
+    private RevTouchSensor toucher1 = null;
+    private boolean touched = false;
 
 
-    @Override
+
+//    public void runTouchBackboard() {
+//        if (toucher1.isPressed()) {
+//            touched = true;
+//        } else {
+//            touched = false;
+//        }
+//    }
+        @Override
     public void initialize() {
         gamepadB = new GamepadEx(gamepad2);
         Intake15455 = new IntakeSubsystem15(hardwareMap, "intake");
         pass15455 = new PassSubsystem15(hardwareMap, "path");
+        toucher1 = hardwareMap.get(RevTouchSensor.class,"ts1");
 
 
-        gamepadB.getGamepadButton(GamepadKeys.Button.A)
+       gamepadB.getGamepadButton(GamepadKeys.Button.A)
+
                 .whenPressed(new GetPixels(Intake15455, pass15455))
                 .whenReleased(new StopGetPixels(Intake15455,pass15455));
 
