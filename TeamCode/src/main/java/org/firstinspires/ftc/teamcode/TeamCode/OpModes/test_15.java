6913b970fabb3a648c6bae3816bef7ece6b08240
TornadoBoah <nboekekitt@gmail.com>
2024-04-24 10:52:35 -0400

    Tried add a touch sensor to ftc lib (Not working yet)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeamCode/OpModes/test_15.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeamCode/OpModes/test_15.java
@@ -22,27 +22,38 @@ public class test_15 extends CommandOpMode {
     private boolean touched = false;
 
 
-
-//    public void runTouchBackboard() {
-//        if (toucher1.isPressed()) {
-//            touched = true;
-//        } else {
-//            touched = false;
-//        }
-//    }
-        @Override
+        public void runTouchBackboard() {
+        if (toucher1.isPressed()) {
+            touched = true;
+        } else {
+            touched = false;
+        }
+    }
+    @Override
     public void initialize() {
         gamepadB = new GamepadEx(gamepad2);
         Intake15455 = new IntakeSubsystem15(hardwareMap, "intake");
         pass15455 = new PassSubsystem15(hardwareMap, "path");
-        toucher1 = hardwareMap.get(RevTouchSensor.class,"ts1");
+        toucher1 = hardwareMap.get(RevTouchSensor.class, "ts1");
 
 
-       gamepadB.getGamepadButton(GamepadKeys.Button.A)
 
-                .whenPressed(new GetPixels(Intake15455, pass15455))
-                .whenReleased(new StopGetPixels(Intake15455,pass15455));
+//       gamepadB.getGamepadButton(GamepadKeys.Button.A)
+//
+//                .whenPressed(new GetPixels(Intake15455, pass15455))
+//                .whenReleased(new StopGetPixels(Intake15455,pass15455));
+    }
+
+    public void run() {
+        if (touched=false) {
+            new StopGetPixels(Intake15455, pass15455).execute();
+
+
+        } else {
+            new GetPixels(Intake15455,pass15455).execute();
+        }
 
 
     }
+
 }
